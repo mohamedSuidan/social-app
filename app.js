5a1eec965cc27e322f91918ed2cbe64fa6af6092
mohamedSuidan <msuidan149@gmail.com>
2022-11-16 05:49:44 -0800

    social-app-project

--- app.js
+++ app.js
@@ -6,12 +6,15 @@ const path = require("path");
 const post = require("./routes/post");
 // app.use(bodyParser.json());
 app.use(cors());
+
 app.use(express.static("public"));
-app.use(express.static("client/build"));
-app.get("*", (req, res, next) => {
-  res.sendFile(path.resolve(__dirname, "client", "build", "index.html"));
-});
-// console.log(path.join(__dirname, "/img"));
+
 app.use(auth);
 app.use(post);
+app.use(express.static("client/build"));
+app.get("*", (req, res) =>
+  res.sendFile(`${__dirname}/client/build/index.html`)
+);
+// console.log(path.join(__dirname, "/img"));
+
 app.listen(4000, () => console.log("server listen"));
